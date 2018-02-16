754a08f55fd2d99744a0e77b39f1753384680e25
wh0am123 <31047829+wh0am123@users.noreply.github.com>
2018-02-16 17:29:37 -0400

    Add files via upload

--- Predicciones/significado_oculto_nombre.js
+++ Predicciones/significado_oculto_nombre.js
@@ -1,25 +1,65 @@
 //significado_oculto_nombre.js
-function lanzar_ajax() {
-    alert( "HAhah" );
-    var ajax;
-    ajax = new XMLHttpRequest();
-    if ( ajax ) {
-        if ( ajax.readyState == 4 ) {
-            var main = document.getElementsByTagName( 'main' )[ 0 ];
-            main.innerHTML = '' + ajax.responseText;
-        }
-        var url = 'significado_nombre.html';
-        ajax.open( 'GET', url );
-        ajax.send();
-    } else {
-        alert( "Tu Navegador no soporta este test" );
-    }
-}
+$(document).ready(function() {
+    
+         $('.test1').click(function() {
 
-function cargar() {
-    /*Crear un evento para cada opción de la barra de navegación
-      y una funcion para cada una que carge la pagina correspondiente al test que eligió */
-    var enlace_p = document.getElementsByTagName( 'p' )[ 0 ];
-    enlace_p.addEventListener( 'click', lanzar_ajax, false );
-}
-window.addEventListener( 'load', cargar, false );
+
+                 $('main').load('significado_nombre.html');
+         });
+
+         $('.test2').click(function() {
+
+
+                 $('main').load('significado_zodiaco.html');
+         });
+
+         $('.test3').click(function() {
+
+
+                // $('main').load('significado_nombre.html');
+         });
+
+         $('.test4').click(function() {
+
+
+            //     $('main').load('significado_nombre.html');
+         });
+
+         $('.test5').click(function() {
+
+
+              //   $('main').load('significado_nombre.html');
+         });
+
+         $('.test6').click(function() {
+
+
+                // $('main').load('significado_nombre.html');
+         });
+
+         $('.test7').click(function() {
+
+
+                 //$('main').load('significado_nombre.html');
+         });
+
+         $('.test8').click(function() {
+
+
+                 //$('main').load('significado_nombre.html');
+         });
+
+         $('.test9').click(function() {
+
+
+                 //$('main').load('significado_nombre.html');
+         });
+
+         $('.test10').click(function() {
+
+
+                 //$('main').load('significado_nombre.html');
+         });
+
+
+});
