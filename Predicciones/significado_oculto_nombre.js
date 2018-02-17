596f8144bf14bfd05fd21b8aa49fe9ab21b36621
wh0am123 <31047829+wh0am123@users.noreply.github.com>
2018-02-17 18:11:22 -0400

    Add files via upload

--- Predicciones/significado_oculto_nombre.js
+++ Predicciones/significado_oculto_nombre.js
@@ -16,50 +16,15 @@ $(document).ready(function() {
          $('.test3').click(function() {
 
 
-                // $('main').load('significado_nombre.html');
+                 $('main').load('significado_nombre.html');
          });
 
          $('.test4').click(function() {
 
 
-            //     $('main').load('significado_nombre.html');
+                $('main').load('significado_nombre.html');
          });
 
-         $('.test5').click(function() {
-
-
-              //   $('main').load('significado_nombre.html');
-         });
-
-         $('.test6').click(function() {
-
-
-                // $('main').load('significado_nombre.html');
-         });
-
-         $('.test7').click(function() {
-
-
-                 //$('main').load('significado_nombre.html');
-         });
-
-         $('.test8').click(function() {
-
-
-                 //$('main').load('significado_nombre.html');
-         });
-
-         $('.test9').click(function() {
-
-
-                 //$('main').load('significado_nombre.html');
-         });
-
-         $('.test10').click(function() {
-
-
-                 //$('main').load('significado_nombre.html');
-         });
-
+         
 
 });
